a0f5aea0b02d6886cb2bcd5a0fa3902557ba6ba8
Vijay-Prakash146246 <vp146246@gmail.com>
2023-03-27 17:21:40 +0530

    Multi server Architecture get added

---
+++ MultiserverArch. using kafka/demo/src/main/java/com/example/demo/AppConstants.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class AppConstants
+{
+    public static final String TOPIC_NAME = "javaguides";
+   // public static final String GROUP_ID = "`group_id`";
+   public static final String GROUP_ID = "group_id";
+}
+
---
+++ MultiserverArch. using kafka/demo2/src/main/java/com/example/demo2/AppConstants.java
@@ -0,0 +1,8 @@
+package com.example.demo2;
+
+public class AppConstants
+{
+    public static final String TOPIC_NAME = "javaguides";
+   public static final String GROUP_ID = "group_id1";
+}
+

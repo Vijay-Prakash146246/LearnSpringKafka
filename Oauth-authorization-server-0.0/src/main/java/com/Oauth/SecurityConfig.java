5e1969ccf219eed1f25c20f420dc77dff29c958b
Vijay-Prakash146246 <vp146246@gmail.com>
2023-04-14 17:08:57 +0530

    Google OAuth configuration get added

---
+++ Oauth-authorization-server-0.0/src/main/java/com/Oauth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.Oauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig
+{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+        return http.build();
+    }
+    //Custom Login Page configuration
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests()
+//                .requestMatchers("/oauth_login")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .oauth2Login()
+//                .loginPage("/oauth_login");
+//        return  http.build();
+//    }
+}
